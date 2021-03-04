c1fa31b006a180bd59c885875b7b2a3373a90389
bhuridech <bhuridech@gmail.com>
2021-03-04 22:46:10 +0700

    :zap: shutdown http server gracefully
    git push origin head

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -1,13 +1,30 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"log"
+	"os"
+	"os/signal"
+)
 
 func main() {
 	app := fiber.New()
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, os.Interrupt)
+	go func() {
+		_ = <-sc
+		log.Println("gracefully shutting down")
+		_ = app.Shutdown()
+	}()
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, world :)")
 	})
 
-	app.Listen(":3000")
+	err := app.Listen(":3000")
+	if err != nil {
+		log.Panic(err)
+	}
+	log.Println("cleanup task")
 }
