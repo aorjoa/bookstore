7bd7bdbc991aea92959d5762d98bb4d2c7a93b31
bhuridech <bhuridech@gmail.com>
2021-03-07 14:18:01 +0700

    :card_file_box: move unique contrain to ISBN field

--- module/book/entity/book.go
+++ module/book/entity/book.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 type Book struct {
 	gorm.Model
 	Name     *string
-	ISBN     *string
+	ISBN     *string `gorm:"unique"`
 	Language *string
-	Status   *bool `gorm:"unique"`
+	Status   *bool
 }
